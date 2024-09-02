64980205cc0ecdc19f7ac246e15f1e0cd949007b
wahyutc <hawkxc068@gmail.com>
2024-09-02 10:42:56 +0700

    Navbar

--- src/Components/Drawer.jsx
+++ src/Components/Drawer.jsx
@@ -4,7 +4,7 @@ const Drawer = ({ SideBarLists, refLists, reference, currentRefIndex }) => {
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
         <label
-          className="fixed flex flex-col items-center justify-center w-10 align-middle transition-all duration-150 bg-white rounded-md md:hidden aspect-square hover:shadow-md active:scale-95 active:shadow-lg top-6 right-8"
+          className="fixed flex flex-col items-center justify-center w-10 align-middle transition-all duration-150 bg-white rounded-md md:hidden aspect-square hover:shadow-md active:scale-95 active:shadow-lg top-3 right-8"
           htmlFor="my-drawer"
         >
           <i className="ri-menu-4-line text-slate-800"></i>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,9 +4,19 @@ import Drawer from "./Drawer";
 
 const Navbar = ({ SideBarLists, refLists, reference, currentRefIndex }) => {
   return (
-    <nav className="fixed top-0 flex flex-row items-center justify-between w-full py-5 md:z-50 md:px-8 max-sm:px-4">
-      <Image src={B} className={"w-12 white-image"} alt={"Bonstom Image"} />
-      <ul className="flex flex-row gap-6 text-white max-sm:hidden">
+    <nav className="fixed top-0 flex flex-row items-center justify-between w-full py-2 transition-all duration-200 md:z-50 md:px-8 max-sm:px-4 md:opacity-70 md:hover:opacity-100">
+      <Image
+        src={B}
+        className={`w-12 duration-300 transition-all ${
+          currentRefIndex > 0 ? "" : "white-image"
+        }`}
+        alt={"Bonstom Image"}
+      />
+      <ul
+        className={`flex flex-row gap-6 max-sm:hidden duration-300 transition-all ${
+          currentRefIndex > 0 ? "text-black" : "text-white"
+        }`}
+      >
         {SideBarLists.filter((_, index) => index !== 0).map((item) => (
           <li
             key={item.name}
